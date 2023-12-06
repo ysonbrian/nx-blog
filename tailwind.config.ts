2cdb3ff2991ec21cc60d357e152d28dda3386350
ysonbrian <ysonbrian@gmail.com>
2023-12-06 17:36:30 +0900

    Initialize Settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,11 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      theme: {
+        container: {
+          center: true,
+        },
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
