ecec6c11f4f8d4ec5ad7b0becc96083695451148
ysonbrian <ysonbrian@gmail.com>
2023-12-08 23:47:59 +0900

    NavList added

--- app/ui/Nav.tsx
+++ app/ui/Nav.tsx
@@ -1,7 +1,26 @@
+import Link from 'next/link'
 import React from 'react'
 
+const NavList = [
+  { name: 'home', src: 'home' },
+  { name: 'about', src: 'about' },
+  { name: 'posts', src: 'posts' },
+  { name: 'contract', src: 'contract' },
+]
+
 const Nav = () => {
-  return <div>This is Nav Page</div>
+  return (
+    <div className={`flex justify-between p-6`}>
+      <div>NX'sBlog</div>
+      <ul className={`flex gap-2`}>
+        {NavList.map((list) => (
+          <Link key={list.name} href={`/${list.src}`}>
+            {list.name}
+          </Link>
+        ))}
+      </ul>
+    </div>
+  )
 }
 
 export default Nav
