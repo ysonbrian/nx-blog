f91908431e235d7c1632ff8be4d2edae87bb7da0
ysonbrian <ysonbrian@gmail.com>
2023-12-09 22:12:40 +0900

    Header 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Home() {
-  return <main className='container'>Welcome to my NX Blog!</main>
+  return (
+    <main className='container mx-auto'>
+      <div className={`flex flex-col justify-center items-center gap-1`}>
+        <div>Image</div>
+        <div className={`text-xl font-black`}>Hi, I'm Crypto313</div>
+        <div className={`text-lg font-bold`}>Full-stack engineer</div>
+        <Link
+          href={`/contact`}
+          className={`p-3 bg-amber-400 rounded-xl font-bold`}
+        >
+          Contact Me
+        </Link>
+      </div>
+    </main>
+  )
 }
